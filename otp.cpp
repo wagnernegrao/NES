1a7ee1be6e41a8905b559de2de1e2728ebc6b4de
wagner <wagnerfelidre@gmail.com>
2019-10-13 14:39:30 -0300

    add create open text

--- otp.cpp
+++ otp.cpp
@@ -1,10 +1,31 @@
-# include "stdio.h"
-# include "stdlib.h"
-# include "string.h"
+#include <iostream> 
+#include <bitset> // Para conversao de string para binario
+#include <string>
+#include <string>
+#include <ctime>
+#include <stdio.h>
+#include <stdlib.h>
 
-# define DEFAULT_KEY 7
+using namespace std;
+
+
 
 int encrypt(int len){
     int i;
+
+}
+
+string createOpenText(string text){
+    //string test1("wagner");
+    string text_bin = "";
+    for(int i = 0; i < text.size(); i++){
+        bitset<8> bitset(text[i]);
+
+        //cout << "c = " << c << endl;
+
+        text_bin = text_bin + bitset.to_string<char,std::string::traits_type,std::string::allocator_type>();
     
+    //cout << text_bin << endl;
+
+    return(text_bin);
 }
