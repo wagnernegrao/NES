0a9e6150aea6fd4b81a1720b03f21005a8f8399f
wagner <wagnerfelidre@gmail.com>
2019-10-14 13:44:48 -0300

    add new module

--- 3DES/3DES.py
+++ 3DES/3DES.py
@@ -5,16 +5,16 @@ from cryptography.hazmat.backends import default_backend
 
 key = os.urandom(16)
 beckend = default_backend()
-text = "a secret message"
+text = b"Aula do samarone"
 
 cipher = Cipher(algorithms.TripleDES(key), modes.ECB(), backend=beckend)
 
 encryptor = cipher.encryptor()
 
-ct = encryptor.update(b"{text}") + encryptor.finalize()
+ct = encryptor.update(text) + encryptor.finalize()
 
 decryptor = cipher.decryptor()
 
 print(f"Texto Encriptado: {ct}")
 
-print(f"Text Decriptado{decryptor.update(ct) + decryptor.finalize()}")
+print(f"Text Decriptado: {decryptor.update(ct) + decryptor.finalize()}")
