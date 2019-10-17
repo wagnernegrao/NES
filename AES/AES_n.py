32bc5ee0f0df04ccadc1f11ff3daa6849ebf4012
wagner <wagnerfelidre@gmail.com>
2019-10-16 22:25:19 -0300

    add pad

---
+++ AES/AES_n.py
@@ -0,0 +1,22 @@
+from Crypto.Cipher import AES
+
+key = b'aula do samarone'
+
+# message = str(input())
+message = '6 semestre'
+
+
+cipher = AES.new(key)  # Obtemos um objeto para podermos criptografar
+
+
+def pad(s):
+    '''
+    recebe o comprimento da mensagem e adiciona a quantidade de *
+    para tornalauniformente divisilvel por 16, isso ocorre por causa do aes que
+    tem dados de comprimento de 16 bits
+    '''
+    value = s + ((16 - len(s) % 16) * '{')
+
+    return(value)
+
+
