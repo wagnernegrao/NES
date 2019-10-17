98df8ce5c28dfc7a47084cb8dc90cbece526a95a
wagner <wagnerfelidre@gmail.com>
2019-10-16 23:22:32 -0300

    add encrypt

--- 3DES/3DES_N.py
+++ 3DES/3DES_N.py
@@ -4,9 +4,17 @@ key = 'aula do samarone'
 
 message = 'espero que funcione'
 
+cipher = DES.new(key, DES.MODE_ECB)
 
 def pad(message):
 
     text = message + ((8 - len(message) % 8) * ' ')
 
     return(text)
+
+text = pad(message)
+print(f"msg {len(message)}  - pad {len(text)}", )
+
+def encrypt(text):
+    return(cipher.encrypt(text))
+
