25bdc48f243e0e33ad734bf7a4fda77bfd48d47e
wagnernegrao <wagnerfelidre@gmail.com>
2019-10-23 08:45:14 -0300

    New pseudorandom

---  Pseudorandom Number Generators/pseudorandom.py
+++  Pseudorandom Number Generators/pseudorandom.py
@@ -1,5 +1,7 @@
 import os
 
+# Generate a seed with os.urandom()
+# This seed is converted to int with int.from_bytes()
 
 for i in range(5, 13):
-    print(f'{os.urandom(i)}')  # i is quantity of bits
+    print(f'{int.from_bytes(os.urandom(i), byteorder="big")}')
